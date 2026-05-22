b8b0c6008f47487769497308051ed7a403e72330
Itbakkey <you910023@gmail.com>
2026-05-22 17:41:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
   });
 
   // ── SCROLL REVEAL
-  const observer = new IntersectionObserver(entries => {
+   const observer = new IntersectionObserver(entries => {
     entries.forEach(e => {
       if (e.isIntersecting) {
         e.target.classList.add('visible');
